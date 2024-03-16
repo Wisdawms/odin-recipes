3c81199dc957d97b8af06e02f1dd521753a3dc7b
wisdoms <135806939+Wisdawms@users.noreply.github.com>
2024-03-16 11:48:28 +0200

    feat: added pizza time

---
+++ script.js
@@ -0,0 +1,18 @@
+const foodDiv = document.querySelector('#foodDiv');
+
+const foods = [{
+    'name': 'lasagna',
+    'imageURL': 'https://static01.nyt.com/images/2023/08/31/multimedia/RS-Lasagna-hkjl/RS-Lasagna-hkjl-superJumbo.jpg',
+},
+
+{
+    'name': 'pizza',
+    'imageURL': 'https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg',
+},
+];
+
+foods.map((food) => {
+    foodDiv.innerHTML += `<div class="foodItem" id="${food.name}">
+    <a href="recipes/${food.name}.html"><img src="${food.imageURL}"></a>
+    </div>`;
+});
